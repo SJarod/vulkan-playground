196d1470677991a911f3a8544bd280e3f191b999
SJarod <91637364+SJarod@users.noreply.github.com>
2025-02-24 18:16:56 +0100

    Update context.cpp

--- internal/graphics/context.cpp
+++ internal/graphics/context.cpp
@@ -18,6 +18,7 @@ std::unique_ptr<Context> ContextBuilder::build()
     InstanceBuilder ib;
     ib.setContext(m_product.get());
     ib.setUseReportCallback(false);
+    // TODO : set app name and version (editable)
     m_product->m_instance = ib.build();
     return std::move(m_product);
 }
